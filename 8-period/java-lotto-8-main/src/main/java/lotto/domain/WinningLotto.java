b5803bc82e1dae9212dded804ac0620b099da17c
Jiihyun <cwlgusc@gmail.com>
2025-12-12 18:49:53 +0900

    feat: 당첨번호와 보너스 번호 중복 여부 검증 추가

--- 8-period/java-lotto-8-main/src/main/java/lotto/domain/WinningLotto.java
+++ 8-period/java-lotto-8-main/src/main/java/lotto/domain/WinningLotto.java
@@ -1,13 +1,22 @@
 package lotto.domain;
 
+import lotto.exception.ExceptionMessage;
+
 public class WinningLotto {
 
     private final Lotto winningNumbers;
     private final LottoNumber bonusNumber;
 
     public WinningLotto(Lotto winningNumbers, LottoNumber bonusNumber) {
+        validateBonusNumber(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto winningNumbers, LottoNumber bonusNumber) {
+        if (winningNumbers.hasSameNumber(bonusNumber)) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_LOTTO_NUMBER.getMessage());
+        }
     }
 
     public int calculateMatchingCount(Lotto purchasedLotto) {
