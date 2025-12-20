c1177b8d6bd183b8a7b984ed33ac6211afd56931
Jiihyun <cwlgusc@gmail.com>
2025-12-20 15:31:07 +0900

    feat: 다리 생성 기능 구현
    - 다리 길이 검증 추가
    - 랜덤한 값에 의해 다리 생성

--- 5-period/java-bridge-main/src/main/java/bridge/domain/MovingCommand.java
+++ 5-period/java-bridge-main/src/main/java/bridge/domain/MovingCommand.java
@@ -5,18 +5,28 @@ import java.util.Arrays;
 
 public enum MovingCommand {
 
-    U("위"),
-    D("아래");
+    U("위", 1),
+    D("아래", 0);
 
     private final String description;
+    private final int moveCondition;
 
-    MovingCommand(final String description) {
+    MovingCommand(String description, int moveCondition) {
         this.description = description;
+        this.moveCondition = moveCondition;
     }
 
     public static MovingCommand from(String input) {
         return Arrays.stream(MovingCommand.values())
                 .filter(element -> element.name().equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.MOVING_COMMAND_NOT_EXISTS.getMessage()));
+    }
+
+    public static String fromCondition(int moveCondition) {
+        return Arrays.stream(MovingCommand.values())
+                .filter(command -> command.moveCondition == moveCondition)
+                .map(Enum::name)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.MOVING_COMMAND_NOT_EXISTS.getMessage()));
     }
