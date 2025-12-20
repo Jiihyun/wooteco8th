d7cc299b52f462f2273044da5c9618fd8de9678b
Jiihyun <cwlgusc@gmail.com>
2025-12-20 16:33:25 +0900

    feat: 유저 다리 이동 및 다리 이동 결과 출력 기능 구현

---
+++ 5-period/java-bridge-main/src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,74 @@
+package bridge.view;
+
+import bridge.domain.BridgeGame;
+import bridge.domain.MovingCommand;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public final class OutputView {
+
+    private OutputView() {
+    }
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public static void printMap(BridgeGame bridgeGame) {
+        List<String> up = new ArrayList<>();
+        List<String> down = new ArrayList<>();
+
+        List<String> bridge = bridgeGame.getBridge();
+        List<String> userBridge = bridgeGame.getUserBridge();
+        judgeAnswer(userBridge, bridge, up, down);
+
+        System.out.println("[ " + String.join(" | ", up) + " ]");
+        System.out.println("[ " + String.join(" | ", down) + " ]");
+    }
+
+    private static void judgeAnswer(List<String> userBridge, List<String> bridge, List<String> up, List<String> down) {
+        for (int i = 0; i < userBridge.size(); i++) {
+            String answer = userBridge.get(i);
+            if (answer.equals(bridge.get(i))) {
+                printO(answer, up, down);
+            }
+            if (!answer.equals(bridge.get(i))) {
+                printX(answer, up, down);
+            }
+        }
+    }
+
+    private static void printO(String answer, List<String> up, List<String> down) {
+        if (answer.equals(MovingCommand.U.name())) {
+            up.add("O");
+            down.add(" ");
+        }
+        if (answer.equals(MovingCommand.D.name())) {
+            up.add(" ");
+            down.add("O");
+        }
+    }
+
+    private static void printX(String answer, List<String> up, List<String> down) {
+        if (answer.equals(MovingCommand.U.name())) {
+            up.add("X");
+            down.add(" ");
+        }
+        if (answer.equals(MovingCommand.D.name())) {
+            up.add(" ");
+            down.add("X");
+        }
+    }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public static void printResult() {
+    }
+}
