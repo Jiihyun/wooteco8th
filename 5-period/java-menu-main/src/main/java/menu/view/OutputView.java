a909dad66f6edac0bafd257c727b64dacf24cece
Jiihyun <cwlgusc@gmail.com>
2025-12-30 11:50:02 +0900

    feat: 메뉴 추천 결과 출력 기능 구현

--- 5-period/java-menu-main/src/main/java/menu/view/OutputView.java
+++ 5-period/java-menu-main/src/main/java/menu/view/OutputView.java
@@ -1,5 +1,10 @@
 package menu.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import menu.domain.menu.Menu;
+import menu.domain.menu.MenuCategory;
+
 public final class OutputView {
 
     private OutputView() {
@@ -7,5 +12,32 @@ public final class OutputView {
 
     public static void showIntro() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    public static void showCategory(List<MenuCategory> categories) {
+        System.out.println("""
+                
+                메뉴 추천 결과입니다.
+                [ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]""");
+        List<String> result = new ArrayList<>();
+        result.add("카테고리");
+        for (MenuCategory category : categories) {
+            String description = category.getDescription();
+            result.add(description);
+        }
+        System.out.println("[ " + String.join(" | ", result) + " ]");
+    }
+
+    public static void showRecommendedMenu(String name, List<Menu> menus) {
+        List<String> result = new ArrayList<>();
+        result.add(name);
+        for (Menu menu : menus) {
+            result.add(menu.getName());
+        }
+        System.out.println("[ " + String.join(" | ", result) + " ]");
+    }
+
+    public static void showOutro() {
+        System.out.println("추천을 완료했습니다.");
     }
 }
