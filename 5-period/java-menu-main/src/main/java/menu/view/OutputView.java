1d84447ec5d578604f1249c50a5497b025ce574a
Jiihyun <cwlgusc@gmail.com>
2025-12-30 23:37:32 +0900

    refactor: 출력 포맷팅 방식 변경

--- 5-period/java-menu-main/src/main/java/menu/view/OutputView.java
+++ 5-period/java-menu-main/src/main/java/menu/view/OutputView.java
@@ -1,11 +1,12 @@
 package menu.view;
 
-import java.util.ArrayList;
 import java.util.List;
 import menu.domain.menu.Menu;
 import menu.domain.menu.MenuCategory;
 
 public final class OutputView {
+
+    private static final String DELIMITER = " | ";
 
     private OutputView() {
     }
@@ -19,22 +20,17 @@ public final class OutputView {
                 
                 메뉴 추천 결과입니다.
                 [ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]""");
-        List<String> result = new ArrayList<>();
-        result.add("카테고리");
-        for (MenuCategory category : categories) {
-            String description = category.getDescription();
-            result.add(description);
-        }
-        System.out.println("[ " + String.join(" | ", result) + " ]");
+        List<String> result = categories.stream()
+                .map(MenuCategory::getDescription)
+                .toList();
+        System.out.println("[ 카테고리 | %s ]".formatted(String.join(DELIMITER, result)));
     }
 
     public static void showRecommendedMenu(String name, List<Menu> menus) {
-        List<String> result = new ArrayList<>();
-        result.add(name);
-        for (Menu menu : menus) {
-            result.add(menu.getName());
-        }
-        System.out.println("[ " + String.join(" | ", result) + " ]");
+        List<String> result = menus.stream()
+                .map(Menu::getName)
+                .toList();
+        System.out.println("[ %s | %s ]".formatted(name, String.join(DELIMITER, result)));
     }
 
     public static void showOutro() {
