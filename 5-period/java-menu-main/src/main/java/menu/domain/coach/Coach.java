d5e9360e8ce386613dea2a89436428f6e560fc33
Jiihyun <cwlgusc@gmail.com>
2025-12-30 10:14:15 +0900

    feat: 못 먹는 메뉴 검증 기능 구현
    - 메뉴 범위 검증
    - 중복된 메뉴 검증

--- 5-period/java-menu-main/src/main/java/menu/domain/coach/Coach.java
+++ 5-period/java-menu-main/src/main/java/menu/domain/coach/Coach.java
@@ -3,17 +3,55 @@ package menu.domain.coach;
 import java.util.List;
 import java.util.Objects;
 import menu.domain.menu.Menu;
+import menu.exception.ExceptionMessage;
 
 public class Coach {
+
+    private static final int MAX_MENU_SIZE = 2;
 
     private final Name name;
     private final List<Menu> cantEatMenus;
 
     public Coach(Name name, List<String> cantEatMenus) {
+        validateMenu(cantEatMenus);
         this.name = name;
-        this.cantEatMenus = cantEatMenus.stream()
+        this.cantEatMenus = initCantEatMenus(cantEatMenus);
+    }
+
+    private List<Menu> initCantEatMenus(List<String> cantEatMenus) {
+        if (cantEatMenus.size() == 1 && cantEatMenus.getFirst().isEmpty()) {
+            return List.of();
+        }
+        return cantEatMenus.stream()
                 .map(Menu::from)
                 .toList();
+    }
+
+    private void validateMenu(List<String> cantEatMenus) {
+        validateSize(cantEatMenus);
+        validateUniqueMenu(cantEatMenus);
+    }
+
+    private void validateSize(List<String> cantEatMenus) {
+        if (isOutOfRange(cantEatMenus.size())) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_MENU_SIZE.getMessage());
+        }
+    }
+
+    private boolean isOutOfRange(int size) {
+        return size > MAX_MENU_SIZE;
+    }
+
+    public void validateUniqueMenu(List<String> cantEatMenus) {
+        if (isDuplicated(cantEatMenus)) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_MENU.getMessage());
+        }
+    }
+
+    private boolean isDuplicated(List<String> cantEatMenus) {
+        return cantEatMenus.stream()
+                .distinct()
+                .count() != cantEatMenus.size();
     }
 
     @Override
--- 5-period/java-menu-main/src/main/java/menu/exception/ExceptionMessage.java
+++ 5-period/java-menu-main/src/main/java/menu/exception/ExceptionMessage.java
@@ -6,8 +6,10 @@ public enum ExceptionMessage {
     INVALID_NAME("유효하지 않은 이름입니다."),
     INVALID_FORMAT("유효하지 않은 형식입니다."),
     INVALID_COACH_SIZE("유효하지 않은 코치 인원 범위입니다."),
+    INVALID_MENU_SIZE("유효하지 않은 메뉴 범위입니다."),
     MENU_NOT_EXISTS("존재하지 않는 메뉴입니다."),
     DUPLICATED_NAME("중복된 코치 이름입니다."),
+    DUPLICATED_MENU("중복된 메뉴입니다."),
     ;
 
     private static final String ERROR_PREFIX = "[ERROR] ";
