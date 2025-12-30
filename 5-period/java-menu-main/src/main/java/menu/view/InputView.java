eafe6a6e7ed75a89559d8471922a8b9458bbe79e
Jiihyun <cwlgusc@gmail.com>
2025-12-30 09:19:23 +0900

    feat: 못 먹는 메뉴 입력 기능 구현

--- 5-period/java-menu-main/src/main/java/menu/view/InputView.java
+++ 5-period/java-menu-main/src/main/java/menu/view/InputView.java
@@ -13,21 +13,27 @@ public final class InputView {
     private InputView() {
     }
 
-    public List<String> readCoachName() {
+    public static List<String> readCoachName() {
         System.out.println(NEW_LINE + "코치의 이름을 입력해 주세요. (, 로 구분)");
         String input = readLine();
-        return Parser.parseByDelimiter(input, DELIMITER);
-    }
-
-    private static String readLine() {
-        String input = Console.readLine().strip();
         validateInput(input);
-        return input;
+        return Parser.parseByDelimiter(input, DELIMITER);
     }
 
     private static void validateInput(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(ExceptionMessage.INPUT_BLANK.getMessage());
         }
+    }
+
+    public static List<String> readCantEatMenu(String name) {
+        System.out.println(NEW_LINE + "%s(이)가 못 먹는 메뉴를 입력해 주세요.".formatted(name));
+        String input = readLine();
+        return Parser.parseByDelimiter(input, DELIMITER);
+    }
+
+
+    private static String readLine() {
+        return Console.readLine().strip();
     }
 }
