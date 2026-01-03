2d8ef26e203ff1ced4226408afdbc78a484cb3b9
Jiihyun <cwlgusc@gmail.com>
2026-01-03 17:25:35 +0900

    refactor: 컨트롤러 메서드 분리

--- 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/controller/PairController.java
+++ 4-period/java-pairmatching-precourse-main/src/main/java/pairmatching/controller/PairController.java
@@ -17,36 +17,61 @@ public class PairController {
     public void run() {
         MatchingResults matchingResults = new MatchingResults();
         MatchingMachine matchingMachine = new MatchingMachine(matchingResults);
+        runByCommand(matchingResults, matchingMachine);
+    }
+
+    private void runByCommand(MatchingResults matchingResults, MatchingMachine matchingMachine) {
         while (true) {
             GameCommand gameCommand = RetryHandler.retryOnInvalidInput(InputView::readCommand);
             if (gameCommand.isQuit()) {
                 return;
             }
-            if (gameCommand.isMatching()) {
-                MatchingRequest matchingRequest = RetryHandler.retryOnInvalidInput(InputView::readMatchingInfo);
-                RequiredMatchingInfo requiredMatchingInfo = new RequiredMatchingInfo(matchingRequest.course(), matchingRequest.level(), matchingRequest.mission());
-                if (matchingResults.existsByRequiredInfo(requiredMatchingInfo)) {
-                    AnswerCommand answerCommand = RetryHandler.retryOnInvalidInput(InputView::readRematchingAnswer);
-                    while (answerCommand.isNo()) {
-                        matchingRequest = RetryHandler.retryOnInvalidInput(InputView::readMatchingInfo);
-                        requiredMatchingInfo = new RequiredMatchingInfo(matchingRequest.course(), matchingRequest.level(), matchingRequest.mission());
-                        if (matchingResults.existsByRequiredInfo(requiredMatchingInfo)) {
-                            answerCommand = RetryHandler.retryOnInvalidInput(InputView::readRematchingAnswer);
-                        }
-                    }
-                }
-                Pairs pairs = matchingMachine.match(requiredMatchingInfo);
-                OutputView.showPairs(pairs);
+            matchByCommand(gameCommand, matchingResults, matchingMachine);
+            RetryHandler.retryOnInvalidInput(() -> findByCommand(gameCommand, matchingResults));
+            resetByCommand(gameCommand, matchingResults);
+        }
+    }
+
+    private void matchByCommand(GameCommand gameCommand, MatchingResults matchingResults, MatchingMachine matchingMachine) {
+        if (gameCommand.isMatching()) {
+            MatchingRequest matchingRequest = RetryHandler.retryOnInvalidInput(InputView::readMatchingInfo);
+            RequiredMatchingInfo requiredMatchingInfo = new RequiredMatchingInfo(matchingRequest.course(), matchingRequest.level(), matchingRequest.mission());
+            if (matchingResults.existsByRequiredInfo(requiredMatchingInfo)) {
+                requiredMatchingInfo = readRematchInfo(matchingResults, requiredMatchingInfo);
             }
-            if (gameCommand.isFind()) {
-                MatchingRequest matchingRequest = RetryHandler.retryOnInvalidInput(InputView::readMatchingInfo);
-                RequiredMatchingInfo requiredMatchingInfo = new RequiredMatchingInfo(matchingRequest.course(), matchingRequest.level(), matchingRequest.mission());
-                MatchingResult matchingResult = matchingResults.findByRequiredInfo(requiredMatchingInfo);
-                OutputView.showMatchingResult(matchingResult);
+            Pairs pairs = matchingMachine.match(requiredMatchingInfo);
+            OutputView.showPairs(pairs);
+        }
+    }
+
+    private RequiredMatchingInfo readRematchInfo(MatchingResults matchingResults, RequiredMatchingInfo requiredMatchingInfo) {
+        MatchingRequest matchingRequest;
+        AnswerCommand answerCommand = RetryHandler.retryOnInvalidInput(InputView::readRematchingAnswer);
+        while (answerCommand.isNo()) {
+            matchingRequest = RetryHandler.retryOnInvalidInput(InputView::readMatchingInfo);
+            requiredMatchingInfo = new RequiredMatchingInfo(matchingRequest.course(), matchingRequest.level(), matchingRequest.mission());
+            if (matchingResults.existsByRequiredInfo(requiredMatchingInfo)) {
+                answerCommand = RetryHandler.retryOnInvalidInput(InputView::readRematchingAnswer);
             }
-            if (gameCommand.isReset()) {
-                matchingResults.reset();
-            }
+        }
+        return requiredMatchingInfo;
+    }
+
+    private void findByCommand(GameCommand gameCommand, MatchingResults matchingResults) {
+        if (gameCommand.isFind()) {
+            MatchingRequest matchingRequest = RetryHandler.retryOnInvalidInput(InputView::readMatchingInfo);
+            RequiredMatchingInfo requiredMatchingInfo = new RequiredMatchingInfo(
+                    matchingRequest.course(),
+                    matchingRequest.level(),
+                    matchingRequest.mission());
+            MatchingResult matchingResult = matchingResults.findByRequiredInfo(requiredMatchingInfo);
+            OutputView.showMatchingResult(matchingResult);
+        }
+    }
+
+    private void resetByCommand(GameCommand gameCommand, MatchingResults matchingResults) {
+        if (gameCommand.isReset()) {
+            matchingResults.reset();
         }
     }
 }
