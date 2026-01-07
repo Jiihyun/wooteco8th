3858db93a85aff71022e05509301caa7fbc6d5ff
Jiihyun <cwlgusc@gmail.com>
2026-01-07 17:43:46 +0900

    feat: 닉네임 형식 검증 추가

--- src/main/java/oncall/domain/Nickname.java
+++ src/main/java/oncall/domain/Nickname.java
@@ -1,18 +1,29 @@
 package oncall.domain;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import oncall.exception.ExceptionMessage;
 
 public class Nickname {
 
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
+    private static final Pattern FORMAT = Pattern.compile("^([가-힣]+)$");
 
     private final String value;
 
     public Nickname(String value) {
         validateLength(value);
+        validateFormat(value);
         this.value = value;
+    }
+
+    private static void validateFormat(String value) {
+        Matcher matcher = FORMAT.matcher(value);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_NICKNAME.getMessage());
+        }
     }
 
     private void validateLength(String value) {
