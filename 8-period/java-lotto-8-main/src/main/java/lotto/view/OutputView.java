71b600fbfc868abc50ca14ca8bbc73ad0de63e8a
Jiihyun <cwlgusc@gmail.com>
2026-01-09 15:18:20 +0900

    feat: 당첨 통계 출력 기능 구현

--- 8-period/java-lotto-8-main/src/main/java/lotto/view/OutputView.java
+++ 8-period/java-lotto-8-main/src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,22 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 public final class OutputView {
 
     private static final String NEW_LINE = System.lineSeparator();
+    private static final String STATISTICS_FORMAT = """
+            당첨 통계
+            ---
+            3개 일치 (5,000원) - %d개
+            4개 일치 (50,000원) - %d개
+            5개 일치 (1,500,000원) - %d개
+            5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
+            6개 일치 (2,000,000,000원) - %d개
+            총 수익률은 %,.1f%%입니다.""";
 
     private OutputView() {
     }
@@ -15,5 +26,16 @@ public final class OutputView {
         purchasedLottos.stream()
                 .map(Lotto::getNumbers)
                 .forEach(System.out::println);
+    }
+
+    public static void showStatistics(Map<Rank, Integer> histories, double profitRate) {
+        System.out.println(STATISTICS_FORMAT.formatted(
+                histories.get(Rank.FIFTH),
+                histories.get(Rank.FOURTH),
+                histories.get(Rank.THIRD),
+                histories.get(Rank.SECOND),
+                histories.get(Rank.FIRST),
+                profitRate
+        ));
     }
 }
