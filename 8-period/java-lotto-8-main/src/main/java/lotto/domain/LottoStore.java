3cc85fe38139b34c9fde41bd64cc32e44c272f50
Jiihyun <cwlgusc@gmail.com>
2026-01-09 14:20:09 +0900

    refactor: LottoGenerator static 제거

--- 8-period/java-lotto-8-main/src/main/java/lotto/domain/LottoStore.java
+++ 8-period/java-lotto-8-main/src/main/java/lotto/domain/LottoStore.java
@@ -7,11 +7,17 @@ public class LottoStore {
 
     private static final int LOTTO_PRICE = 1_000;
 
+    private final LottoGenerator lottoGenerator;
+
+    public LottoStore(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
     public List<Lotto> sellLotto(PurchasedAmount purchasedAmount) {
         //TODO: 게터 리팩토링
         int lottoQuantity = purchasedAmount.getValue() / LOTTO_PRICE;
         return IntStream.range(0, lottoQuantity)
-                .mapToObj(i -> LottoGenerator.generate())
+                .mapToObj(i -> lottoGenerator.generate())
                 .map(Lotto::new)
                 .toList();
     }
